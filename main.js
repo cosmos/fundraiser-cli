3b5923d2cf2e9b198b4c0c580a94c08a31dba953
Matt Bell <mappum@gmail.com>
2017-02-27 22:06:53 -0800

    Added intermediate tx and finalize steps

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 'use strict'
 
 const fs = require('fs')
-const { cyan, yellow, green, red } = require('chalk')
+const { bold, cyan, yellow, green, red } = require('chalk')
 const { prompt } = require('inquirer')
-const ora = require('ora')
+const createSpinner = require('ora')
 const struct = require('varstruct')
 const cfr = require('cosmos-fundraiser')
 const pkg = require('./package.json')
@@ -36,12 +36,12 @@ TODO: more information
 )
 
 async function main () {
-  let walletPath = './cosmos_fundraiser.wallet'
-  let wallet = await createOrLoadWallet(walletPath)
-  // TODO: check/wait for unspent txs
-  // TODO: ask for confirmation, finalize (create exodus tx, send)
+  let testnet = process.env.NODE_ENV === 'development'
+  let walletPath = process.argv[2] || './cosmos_fundraiser.wallet'
+  let wallet = await createOrLoadWallet(walletPath, testnet)
+  let tx = await waitForTx(wallet.addresses.bitcoin, testnet)
+  await finalize(wallet, tx, testnet)
 }
-
 
 const Wallet = struct([
   { name: 'encryptedSeed', type: struct.VarBuffer(struct.Byte) },
@@ -49,19 +49,19 @@ const Wallet = struct([
   { name: 'iv', type: struct.VarBuffer(struct.Byte) }
 ])
 
-async function createOrLoadWallet (path) {
+async function createOrLoadWallet (path, testnet) {
   try {
     // test if we wallet exists and we have access
     fs.accessSync(path)
-    console.log(`Found wallet file: ${path}`)
-    return await loadWallet(path)
+    console.log(`Found existing wallet file: ${path}`)
+    return await loadWallet(path, testnet)
   } catch (err) {
     if (err.code !== 'ENOENT') throw err
-    return await createWallet(path)
+    return await createWallet(path, testnet)
   }
 }
 
-async function loadWallet (path) {
+async function loadWallet (path, testnet) {
   let walletBytes = fs.readFileSync(path)
   let wallet = Wallet.decode(walletBytes)
   while (true) {
@@ -72,14 +72,14 @@ async function loadWallet (path) {
     })
     try {
       let seed = cfr.decryptSeed(wallet, password)
-      return cfr.deriveWallet(seed)
+      return cfr.deriveWallet(seed, testnet)
     } catch (err) {
       console.log(red('Incorrect password'))
     }
   }
 }
 
-async function createWallet (path) {
+async function createWallet (path, testnet) {
   console.log(
     `We will now create a Cosmos wallet and a password.\n`,
     red(`WARNING: If you lose your password, you will lose access to your Atoms. There is no way to recover or reset your password. Write down your password and DO NOT LOSE IT!`)
@@ -87,12 +87,12 @@ async function createWallet (path) {
 
   let password = await passwordCreatePrompt()
   let seed = cfr.generateSeed()
-  let wallet = cfr.deriveWallet(seed)
-  let encryptSpinner = ora('Encrypting wallet...').start()
+  let wallet = cfr.deriveWallet(seed, testnet)
+  let encryptSpinner = createSpinner('Encrypting wallet...').start()
   let encryptedSeed = cfr.encryptSeed(seed, password)
   encryptSpinner.succeed('Encrypted wallet')
 
-  let saveSpinner = ora(`Saving wallet...`).start()
+  let saveSpinner = createSpinner(`Saving wallet...`).start()
   let walletBytes = Wallet.encode(encryptedSeed)
   fs.writeFileSync(path, walletBytes)
   saveSpinner.succeed(`Saved wallet to ${path}`)
@@ -122,6 +122,72 @@ async function passwordCreatePrompt () {
     }
   } while (confirm !== password)
   return password
+}
+
+function waitForTx (address, testnet) {
+  console.log(`
+${bold('Exchange rate:')} 1 BTC : ${cfr.bitcoin.ATOMS_PER_BTC} ATOM
+${bold('Minimum amount:')} ${cfr.bitcoin.MINIMUM_AMOUNT / 1e8} BTC
+
+Your intermediate Bitcoin address is:
+${cyan(address)}
+
+Send coins to this address to continue with your purchase.
+This address is owned by you, so you can get the coins back if you
+change your mind.
+  `)
+  let spinner = createSpinner('Waiting for a transaction...').start()
+  return new Promise((resolve, reject) => {
+    cfr.bitcoin.waitForTx(address, { testnet }, (err, tx) => {
+      if (err) return reject(err)
+      spinner.succeed('Received transaction:\n' + cyan(tx.tx.getId()))
+      resolve(tx)
+    })
+  })
+}
+
+async function finalize (wallet, tx, testnet) {
+  let finalTx = cfr.bitcoin.createFinalTx(wallet, tx, testnet)
+  console.log(`
+Ready to finalize purchase:
+  ${bold('Paid:')} ${finalTx.paidAmount / 1e8} BTC
+  ${bold('Transaction fee:')} ${finalTx.feeAmount / 1e8} BTC
+  ${bold('Purchasing:')} ${finalTx.atomAmount} ATOM
+  ${bold('Cosmos address:')} ${wallet.addresses.cosmos}
+  `)
+
+  let { agree } = await prompt({
+    type: 'confirm',
+    name: 'agree',
+    message: 'Have you read and understand the Terms of Service and Purchase Agreement?',
+    default: false
+  })
+  if (!agree) {
+    console.log(red(`
+You can read the Terms of Service and Purchase Agreement here:
+TODO: links
+    `))
+    return
+  }
+
+  let { confirm } = await prompt({
+    type: 'confirm',
+    name: 'confirm',
+    message: 'Finalize purchase of Atoms? You will NOT be able undo this transaction:',
+    default: false
+  })
+  if (!confirm) return
+
+  let spinner = createSpinner('Broadcasting transaction...')
+  let txid = await new Promise((resolve, reject) => {
+    cfr.bitcoin.pushTx(finalTx.tx, { testnet }, (err, txid) => {
+      if (err) return reject(err)
+      resolve(txid)
+    })
+  })
+  spinner.succeed('Transaction sent!')
+  console.log('TXID: ' + cyan(txid))
+  console.log('Thank you for participating in the Cosmos fundraiser!')
 }
 
 main().catch((err) => { throw err })
