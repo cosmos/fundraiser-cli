e2b04722ef117275e524090edef841ec31361dcf
Ethan Buchman <ethan@coinculture.info>
2017-09-13 00:46:50 -0400

    fuzzer permutations

--- fuzzer/main.go
+++ fuzzer/main.go
@@ -6,9 +6,10 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	cmn "github.com/tendermint/go-common"
-	"github.com/tendermint/go-crypto"
+	crypto "github.com/tendermint/go-crypto"
 	"github.com/tendermint/go-crypto/hd"
 	"github.com/tyler-smith/go-bip39"
 )
@@ -63,7 +64,58 @@ func main() {
 	// Split mnemonic to 12 words
 	mnemonicSlice := strings.Split(mnemonic, " ")
 
-	// For each position,
+	// First try all pairwise swaps
+	fmt.Println("Trying all pairwise swaps of the words. Please be patient...")
+	ch := make(chan []string)
+	go Pairwise(mnemonicSlice, ch)
+	for {
+		newWords, ok := <-ch
+		if !ok {
+			break
+		}
+
+		checkWords(newWords)
+	}
+
+	// Now try all double pairwise swaps
+	fmt.Println("Trying all double pairwise swaps of the words. Please be patient...")
+	ch = make(chan []string)
+	go DoublePairwise(mnemonicSlice, ch)
+	for {
+		newWords, ok := <-ch
+		if !ok {
+			break
+		}
+
+		checkWords(newWords)
+	}
+
+	// Now try all possible permutations.
+	fmt.Println("Trying all permutations of the words. Please be patient...")
+	ch = make(chan []string)
+	go Permute(mnemonicSlice, ch)
+	counter := 0
+	start := time.Now()
+	for {
+		if counter%100000 == 0 {
+			fmt.Printf("Tried %d / %d permutations. Took %v ...\n", counter, 479001600, time.Since(start))
+			start = time.Now()
+		}
+
+		c, ok := <-ch
+		if !ok {
+			break
+		}
+
+		counter += 1
+		go func(words []string) {
+			checkWords(words)
+		}(c)
+	}
+	fmt.Printf("Tried %d permutations. No match found\n", counter)
+
+	// Now try fiddling each position.
+	fmt.Println("Trying fiddling each each of the words, keeping the others constant. Please be patient...")
 	for i := 0; i < 12; i++ {
 		fmt.Println("Fiddling word no", i)
 		// For each word in WordList,
@@ -72,29 +124,108 @@ func main() {
 			copy(c, mnemonicSlice)
 			c[i] = bip39.WordList[j]
 
-			m := strings.Join(c, " ")
-			seed := bip39.NewSeed(m, "")
-			_, priv, ch, _ := hd.ComputeMastersFromSeed(string(seed))
-
-			// Calculate Cosmos Addr
-			{
-				privBytes := hd.DerivePrivateKeyForPath(
-					hd.HexDecode(priv),
-					hd.HexDecode(ch),
-					"44'/118'/0'/0/0",
-				)
-				pubBytes := hd.PubKeyBytesFromPrivKeyBytes(privBytes, true)
-				var pubKey crypto.PubKeySecp256k1
-				copy(pubKey[:], pubBytes)
-				addr := pubKey.Address()
-				addrStr := fmt.Sprintf("0x%X (%v)", addr, m)
-				if flagFilter == "" {
-					fmt.Println(addrStr)
-				} else if strings.Contains(addrStr, flagFilter) {
-					fmt.Println(addrStr)
-				}
-			}
+			checkWords(c)
 		}
 	}
 
+}
+
+func computeAddrString(c []string) string {
+	m := strings.Join(c, " ")
+	seed := bip39.NewSeed(m, "")
+	_, priv, ch, _ := hd.ComputeMastersFromSeed(string(seed))
+
+	// Calculate Cosmos Addr
+	privBytes := hd.DerivePrivateKeyForPath(
+		hd.HexDecode(priv),
+		hd.HexDecode(ch),
+		"44'/118'/0'/0/0",
+	)
+	pubBytes := hd.PubKeyBytesFromPrivKeyBytes(privBytes, true)
+	var pubKey crypto.PubKeySecp256k1
+	copy(pubKey[:], pubBytes)
+	addr := pubKey.Address()
+	addrStr := fmt.Sprintf("0x%X (%v)", addr, m)
+	return addrStr
+}
+
+func Pairwise(list []string, ch chan []string) {
+	newList := make([]string, len(list))
+	for i := 0; i < len(list); i++ {
+		for j := i; j < len(list); j++ {
+			copy(newList, list)
+			newList[i] = list[j]
+			newList[j] = list[i]
+			ch <- newList
+		}
+	}
+	close(ch)
+}
+
+func DoublePairwise(list []string, ch chan []string) {
+	newList := make([]string, len(list))
+	for i := 0; i < len(list); i++ {
+		for j := i; j < len(list); j++ {
+			copy(newList, list)
+			newList[i] = list[j]
+			newList[j] = list[i]
+			ch2 := make(chan []string)
+			go Pairwise(newList, ch2)
+		INNER:
+			for {
+				c, ok := <-ch2
+				if !ok {
+					break INNER
+				}
+				ch <- c
+			}
+		}
+	}
+	close(ch)
+}
+
+func Permute(list []string, ch chan []string) {
+	newList := make([]string, len(list))
+	permute(newList, list, 0, ch)
+	close(ch)
+
+}
+
+func permute(newList, list []string, iterIdx int, ch chan []string) {
+
+	// if we filled the new list, return
+	if iterIdx == len(newList) {
+		return
+	}
+
+	// give each element in the list a chance to be at the front
+	for i := 0; i < len(list); i++ {
+		newList[iterIdx] = list[i]
+
+		// remove the element that is now at the front of the new list
+		l := make([]string, len(list)-1)
+		copy(l, list[:i])
+		copy(l[i:], list[i+1:])
+
+		// recursively call permute
+		permute(newList, l, iterIdx+1, ch)
+
+		// this happens once for every permutation
+		if iterIdx == len(newList)-2 {
+			perm := make([]string, len(newList))
+			// copy it so its not overwritten while waiting to send
+			copy(perm, newList)
+			ch <- perm
+		}
+	}
+}
+
+func checkWords(words []string) {
+	addrStr := computeAddrString(words)
+	if flagFilter == "" {
+		fmt.Println(addrStr)
+	} else if strings.Contains(addrStr, flagFilter) {
+		fmt.Println("FOUND A MATCH!!!", addrStr)
+		os.Exit(0)
+	}
 }
